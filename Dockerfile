9e001bcce2c3b8ca71430192668f4d8ea9f7b471
SzymonOles <szymon.oles.2k@gmail.com>
2025-04-12 12:46:40 +0200

    sonar changes

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,6 @@ WORKDIR /app
 COPY package*.json ./
 RUN npm install --ignore-scripts
 COPY . .
-RUN npm run build
-
-RUN npm install --ignore-scripts -g serve
+RUN npm run build && npm install --ignore-scripts -g serve
 EXPOSE 3000
 CMD ["serve", "-s", "build","-p","3000"]
--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -1,19 +1,42 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useState, useMemo, useCallback } from 'react';
+import PropTypes from 'prop-types';
 
 const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
     const [cart, setCart] = useState([]);
 
-    const addToCart = product => setCart([...cart, product]);
-    const removeFromCart = id => setCart(cart.filter(item => item.id !== id));
-    const clearCart = () => setCart([]);
+    const addToCart = useCallback(
+        product => setCart(prev => [...prev, product]),
+        []
+    );
+
+    const removeFromCart = useCallback(
+        id => setCart(prev => prev.filter(item => item.id !== id)),
+        []
+    );
+
+    const clearCart = useCallback(
+        () => setCart([]),
+        []
+    );
+
+    const value = useMemo(() => ({
+        cart,
+        addToCart,
+        removeFromCart,
+        clearCart,
+    }), [cart, addToCart, removeFromCart, clearCart]);
 
     return (
-        <CartContext.Provider value={{ cart, addToCart, removeFromCart, clearCart }}>
+        <CartContext.Provider value={value}>
             {children}
         </CartContext.Provider>
     );
+};
+
+CartProvider.propTypes = {
+    children: PropTypes.node.isRequired,
 };
 
 export const useCart = () => useContext(CartContext);
